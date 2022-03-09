2309493e1fcc71255793a1498018964b11a1fd2e
kshitij0209ag <kshitij.agrawal0209@gmail.com>
2022-03-09 19:51:29 +0530

    Java Stdin and Stdout I

---
+++ Ques2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Ques2 {
+    public static void main(String[] args) {
+        // Scanner scanner = new Scanner(System.in);
+        // String myString = scanner.next();
+        // int myInt = scanner.nextInt();
+        // scanner.close();
+
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        // System.out.println("myString is: " + myString);
+        // System.out.println("myInt is: " + myInt);
+    }
+}
+
+
